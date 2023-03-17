0163bddea03c5e5fb105df089a5d36a05179eaf0
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-03-17 16:33:46 -0700

    my commit file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	} else if (L == 0)
 	{
 		printf("Last digit of %d id %d and is 0\n", n, L);
-	} else if (L < 6 && L |= 0)
+	} else if (L < 6 && L != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, L);
 	}
