ae4c3414910c7b668d9924aa85a7168366a6d36f
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-03-17 17:12:38 -0700

    my commit file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char low = 'a';
+
+	for (low = ;a'; low <= 'z'; low++)
+	{
+		if (low != 'q' && low != 'e')
+		{
+			putchar(low);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
