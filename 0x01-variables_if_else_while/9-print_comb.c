d7d4eaaf1876b27d84ae8215b0c867529f827774
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-03-17 17:53:59 -0700

    my commit file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num = o;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
