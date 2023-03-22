2eaf72a38345c5c0dfd6c719cb59309886f42218
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-03-22 02:04:54 -0700

    my commit file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,8 +20,8 @@ void jack_bauer(void)
 		{
 			_putchar((hr / 10) + '0');
 			_putchar((hr % 10) + '0');
-		       	_putchar(':');
-		        _putchar((min / 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
 			_putchar('\n');													}
 	}
