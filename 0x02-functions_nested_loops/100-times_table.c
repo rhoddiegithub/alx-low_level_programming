fdc6bd4aa4bd5bd778dae8483cbd636f9c40e0a9
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-03-22 03:27:28 -0700

    my commit file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,8 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			_putchar('0');												    
+			_putchar('0');
+
 			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
@@ -26,14 +27,14 @@ void print_times_table(int n)
 				}
 				if (prod <= 9)
 				{
-				       	_putchar(' ');
+					_putchar(' ');
 				}
 				if (prod >= 100)
 				{
-				        _putchar((prod / 100) + '0');
+					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (prod <= 99 && prod >=10)
+				else if (prod <= 99 && prod >= 10)
 				{
 					_putchar((prod / 10) + '0');
 				}
