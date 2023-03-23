574184e54ecfae41f07de99228ca8e1fec958bca
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-03-23 14:54:28 -0700

    my commit file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	char a;
+	char b;
+
+	for (a = 0; a <=9; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if(b > 9)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
