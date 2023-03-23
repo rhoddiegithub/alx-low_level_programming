cbfcb754a1f02861dfa6c3b1bcddf10ee2ed985f
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-03-23 15:25:31 -0700

    my commit file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal
+ * @n: is the integer for the parameters of my function
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int a = 0;
+	int b = n;
+
+	if (b > 0)
+	{
+		while (a < b)
+		{
+			_putchar('_');
+			a++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_outchar('\n');
+	}
+}
