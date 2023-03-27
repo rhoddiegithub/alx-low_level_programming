2b81f01d3c8de6eca0cce86533aa7e2fdf1baa5e
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-03-27 14:56:36 -0700

    my commit file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,10 +4,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int_putchar(char c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int_strlen(char *s);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -15,6 +15,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
 
 #endif
