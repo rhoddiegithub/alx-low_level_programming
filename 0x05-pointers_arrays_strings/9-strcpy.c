cd8c976471acc53475950258dd38ca881452e1da
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-03-27 18:14:12 -0700

    my commit file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main."
+
+/**
+ * _strcpy - Copies a string to by @src including the
+ * terminating null byte to a buffer pointed to by @dest
+ * @dest: A buffer to copy the string to
+ * @src: The source string to copy
+ * Return: A pointer to the destination string @dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+
+	}
+	dest[index] = '\0';
+
+	return (dest);
+}
