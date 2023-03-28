b4a970c80c556e02adb7dd30dbc48cd62c41b1af
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-03-27 17:34:43 -0700

    my commit file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses a string
- * @s: Input string
- * Return: String in reverse
+ * puts2 - func that prints 1 char/2 of a string, followed by a new line
+ * @str: declaration of str and parameters for the function puts2
+ * Return: Always 0
  */
 
-void rev_string(char *s)
+void puts2(char *str)
 {
-	char rev;
-	int counter = 0;
 	int i;
+	char c;
 
-	while (s[counter] != '\0')
-		counter++;
-	for (i = 0; i < counter; i++)
+	for (i = 0; str[i] != 0; i++)
 	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+		if (i % 2 == 0)
+		{
+			c = str[i];
+			_putchar(c);
+		}
 	}
 }
