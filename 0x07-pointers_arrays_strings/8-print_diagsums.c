a850a063762cb2352920193e6589862132692b29
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-04 16:03:07 -0700

    my commit file

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,7 +3,7 @@
 
 /**
  * print_diagsums - function that prints the sum of the
- * two diagonals of a square matrix of integers
+ *     two diagonals of a square matrix of integers
  * @a: the matrix
  * @size: the size of the matrix
  */
@@ -14,11 +14,11 @@ void print_diagsums(int *a, int size)
 	int sum_2 = 0;
 	int i;
 
-	for (i = 0; < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		sum_1 += *(a + i size + i);
+		sum_1 += *(a + i * size + i);
 
-		sum_2 += *(a + i size + (size - i - 1));
+		sum_2 += *(a + i * size + (size - i - 1));
 	}
 	printf("%d, %d\n", sum_1, sum_2);
 }
