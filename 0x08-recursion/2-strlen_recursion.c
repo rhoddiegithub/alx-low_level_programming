fbffbffeaad2f118be57894604ae28f6ffaa0d5a
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-06 17:18:39 -0700

    my commit file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: string to measure
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+	}
+	else
+	{
+		i++;
+		i + = _strlen_recursion(s + 1);
+	}
+	return (i);
+}
