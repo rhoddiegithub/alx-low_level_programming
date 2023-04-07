21071e1c4b4cc20439387a1747ebb78af91dc738
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-06 19:20:01 -0700

    my commit file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -28,7 +28,7 @@ int string_match(char *s1, char *s2, char *after_card)
 	}
 	if (*s1 == *s2)
 	{
-		return (string_match(s1 + 1, s2 + 1, after_card);
+		return (string_match(s1 + 1, s2 + 1, after_card));
 	}
 }
 
