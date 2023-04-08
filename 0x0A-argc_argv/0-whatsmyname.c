656ad1b357c67f3915032959d30177b950deb481
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-08 05:06:51 -0700

    my commit file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print program name
+ * @argc: counts the number of arguments
+ * @argv: pointer to array of argument
+ * Return: 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
