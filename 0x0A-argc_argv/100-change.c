9f5c204187097d49f7397a71cb639f788bc748ad
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-10 11:19:35 -0700

    my commit file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,25 +12,29 @@
 
 int main(int argc, char *argv[])
 {
-	int num, coins = 0;
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	int cents = atoi(argv[1]);
 
-	num = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-	coins += num / 25;
-	num = num % 25;
-	coins += n / 10;
-	num = num % 10;
-	coins += num / 5;
-	num = num % 5;
-	coins += num / 2;
-	num = num % 2;
-	coins += num / 1;
+	int coins = 0;
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+	coins += cents;
 
 	printf("%d\n", coins);
 	return (0);
