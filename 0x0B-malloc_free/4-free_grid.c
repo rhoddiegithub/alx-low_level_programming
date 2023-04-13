270250991e7818f3727ac82c4d716ebdc97afcad
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-12 19:44:57 -0700

    my commit file

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+ /**
+ * free_grid - frees a 2 dimensional grid
+ * previously created by your alloc_grid function
+ * @grid: pointer to 2D array
+ * @height: height of array
+ */
+
+void free_grid(int **grid, int height)
+{
+	int index;
+
+	for (index = 0; index < height; index++)
+	{
+		free(grid[index]);
+	}
+	free(grid);
+}
