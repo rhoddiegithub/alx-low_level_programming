76c1551299592849a53f019784c4ab56098cc775
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-13 19:51:01 -0700

    my commit file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,28 +4,29 @@
 #include <ctype.h>
 
 /**
- * _iszero - determines if any number is zero
+ * _is_zero - determines if any number is zero
  * @argv: argument vector.
+ * 
  * Return: no return.
  */
-void _iszero(char *argv[])
+void _is_zero(char *argv[])
 {
-	int index, isn1 = 1, isn2 = 1;
+	int i, isn1 = 1, isn2 = 1;
 
-	for (index = 0; argv[1][index]; index++)
-		if (argv[1][index] != '0')
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
 		{
 			isn1 = 0;
 			break;
 		}
-
-	for (index = 0; argv[2][index]; index++)
-		if (argv[2][index] != '0')
-		{
+	
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{	
 			isn2 = 0;
 			break;
 		}
-
+	
 	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
@@ -34,61 +35,65 @@ void _iszero(char *argv[])
 }
 
 /**
- *  _init_array - set memery to zero in a new array
- * @arr: char array.
- * @len: length of the char array.
+ *  _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ * 
  * Return: pointer of a char array.
  */
-char *_init_array(char *arr, int len)
+char *_initialize_array(char *ar, int lar)
 {
-		int index = 0;
-
-		for (index = 0; index < len; index++)
-			arr[index] = '0';
-		arr[len] = '\0';
-		return (arr);
+		int i = 0;
+		
+		for (i = 0; i < lar; i++)
+			ar[i] = '0';
+		ar[lar] = '\0';
+		return (ar);
 }
 
 /**
- * _check_num - determines length of the number
+ * _checknum - determines length of the number
  * and checks if number is in base 10.
  * @argv: arguments vector.
  * @n: row of the array.
+ * 
  * Return: length of the number.
  */
-int _check_num(char *argv[], int n)
+int _checknum(char *argv[], int n)
 {
-	int len;
-
-	for (len = 0; argv[n][len]; len++)
-		if (!isdigit(argv[n][len]))
+	int ln;
+	
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
-
-	return (len);
+	
+	return (ln);
 }
 
 /**
- * main - program that multiplies two positive numbers.
+ * main - Entry point.
+ * program that multiplies two positive numbers.
  * @argc: number of arguments.
  * @argv: arguments vector.
- * Return: 0
+ * 
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int len1, len2, lnout, add, addl, i, j, k, ca;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
 	char *nout;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	len1 = _check_num(argv, 1), len2 = _check_num(argv, 2);
-	_iszero(argv), lnout = len1 + len2, nout = malloc(lnout + 1);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
-	nout = _init_array(nout, lnout);
-	k = lnout - 1, i = len1 - 1, j = len2 - 1, ca = addl = 0;
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
 		if (i < 0)
@@ -100,15 +105,15 @@ int main(int argc, char *argv[])
 					nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
-			i = len1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
 		if (j < 0)
 		{
 			if (nout[0] != '0')
 				break;
 			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _init_array(nout, lnout);
-			k = lnout - 1, i = len1 - 1, j = len2 - 1, ca = addl = 0;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
 		{
