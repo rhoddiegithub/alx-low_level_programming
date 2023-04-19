b79e7702fc05ea4de25269295568441d2b20145e
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-18 17:14:10 -0700

    my commit file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog
+ * @name: The first member
+ * @age: The second member
+ * @owner: The third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
