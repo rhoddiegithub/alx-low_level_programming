7fb2cb7e7fbffd707f5d8a363c6930cef0d97c06
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-18 17:36:34 -0700

    my commit file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Free instance of struct dog
+ * @d: instance to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (d->name)
+		free(d->name);
+
+	if (d->owner
+		free(d->owner);
+
+	free(d);
+}
