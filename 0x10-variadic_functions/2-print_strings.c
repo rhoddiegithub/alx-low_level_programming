2a19c13a03fa1616b8a479f85524dee3df0c397b
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-20 17:09:40 -0700

    my commit file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		}
 
-		if (i < n - 1 && separator != NULL)
+		if (index < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
