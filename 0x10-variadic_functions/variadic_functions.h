aa6fb199717bd13921ed62ec05562da86675ab9a
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-04-20 17:07:52 -0700

    my commit file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,25 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+
+void print_all(const char * const format, ...);
+
+/**
+ * struct datas - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct datas
+{
+	char letter;
+	void (*func)(va_list);
+} data;
+
 
 #endif
