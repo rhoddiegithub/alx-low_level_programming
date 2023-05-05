c7d2c8db00c90745e2834d84885ceab035c8e190
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-05-04 18:03:58 -0700

    my commit file"

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @num: the number which contains the bit
+ * @index: the index at which bit is be found
+ * Return: bit (Success) or -1 (error)
+ */
+
+int get_bit(unsigned long int num, unsigned int index)
+{
+	int num;
+
+	if (index > (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
+	num = (num >> index) & 1;
+	return (num);
+}
