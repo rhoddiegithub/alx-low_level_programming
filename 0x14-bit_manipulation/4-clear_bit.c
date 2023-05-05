27503497fbc43068e1754589edf10b1ccded8b8b
rhoddiegithub <rodiatyinusa22@gmail.com>
2023-05-04 18:18:53 -0700

    my commit file

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * clear_bit - Sets the value of a bit at a given index to 0.
- * @n: pointer to the bit.
+ * @num: pointer to the bit.
  * @index: index to set the value at - indices start at 0.
  * Return: If an error occurs - -1.Otherwise - 1.
  */
